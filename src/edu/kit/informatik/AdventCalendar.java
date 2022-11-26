20234c64260b725079a589be599e2694c5c86c0a
Moritz Gstür <ufgro@student.kit.edu>
2022-11-26 18:45:20 +0000

    Exercise-Template pushed by Artemis

---
+++ src/edu/kit/informatik/AdventCalendar.java
@@ -0,0 +1,99 @@
+package edu.kit.informatik;
+
+/**
+ * TODO Write Javadoc
+ * 
+ * @author
+ * @version
+ */
+public class AdventCalendar {
+
+    /**
+     * TODO Write Javadoc
+     * 
+     * @param sweets
+     */
+    public AdventCalendar(String[] sweets) {
+        // TODO Implement
+        throw new UnsupportedOperationException("Implementation missing");
+    }
+
+    /**
+     * TODO Write Javadoc
+     * 
+     * @return
+     */
+    public int getDay() {
+        // TODO Implement
+        throw new UnsupportedOperationException("Implementation missing");
+    }
+
+    /**
+     * TODO Write Javadoc
+     * 
+     * @return
+     */
+    public boolean nextDay() {
+        // TODO Implement
+        throw new UnsupportedOperationException("Implementation missing");
+    }
+
+    /**
+     * TODO Write Javadoc
+     * 
+     * @param days
+     * @return
+     */
+    public boolean nextDays(int days) {
+        // TODO Implement
+        throw new UnsupportedOperationException("Implementation missing");
+    }
+
+    /**
+     * TODO Write Javadoc
+     * 
+     * @param number
+     * @return
+     */
+    public boolean isDoorOpen(int number) {
+        // TODO Implement
+        throw new UnsupportedOperationException("Implementation missing");
+    }
+
+    /**
+     * TODO Write Javadoc
+     * 
+     * @param number
+     * @return
+     */
+    public String openDoor(int number) {
+        // TODO Implement
+        throw new UnsupportedOperationException("Implementation missing");
+    }
+
+    /**
+     * TODO Write Javadoc
+     * 
+     * @return
+     */
+    public int numberOfUnopenedDoors() {
+        // TODO Implement
+        throw new UnsupportedOperationException("Implementation missing");
+    }
+
+    /**
+     * TODO Write Javadoc
+     */
+    public void reset() {
+        // TODO Implement
+        throw new UnsupportedOperationException("Implementation missing");
+    }
+
+    /**
+     * TODO Write Javadoc
+     */
+    public String toString() {
+        // TODO Implement
+        throw new UnsupportedOperationException("Implementation missing");
+    }
+}
